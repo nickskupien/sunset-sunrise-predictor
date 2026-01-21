e91891e02c8fe8ba67497a90e972f3ee86e3ae5e
Nick Skupien <nicholasskupien@gmail.com>
2026-01-20 20:47:05 -0500

    SUNSET/SUNRISE PREDICTOR: fix eslint for next.js

--- packages/config/eslint/next.mjs
+++ packages/config/eslint/next.mjs
@@ -1,23 +1,23 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { fixupConfigRules } from "@eslint/compat";
 import globals from "globals";
+import next from "@next/eslint-plugin-next";
 
 import base from "./base.mjs";
 
-const flatCompat = new FlatCompat();
-
-/** Next.js + React ESLint config. */
-const config = [
+export default [
   ...base,
-  ...fixupConfigRules(flatCompat.extends("eslint-config-next/core-web-vitals")),
   {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
       globals: {
         ...globals.browser,
       },
     },
+    plugins: {
+      "@next/next": next,
+    },
+    rules: {
+      ...next.configs.recommended.rules,
+      ...next.configs["core-web-vitals"].rules,
+    },
   },
 ];
-
-export default config;
